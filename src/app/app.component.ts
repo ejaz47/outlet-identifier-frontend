b9b8bbaaa4c73acbe955934679afb8d3f870e9f7
Ejaz Ansari <ejazahemed999@gmail.com>
2019-06-30 18:58:43 +0530

    frontend compleete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient,  HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'outlet-identifier-frontend';
+    title = 'outlet-identifier';
+    baseUrl = 'http://localhost:1234';
+    output = '';
+	constructor(private httpClient: HttpClient){}
+
+    get_outlet(addr){
+    	this.output = 'Please wait...';
+    	let params = new HttpParams();
+	    params = params.append('address', addr);
+
+	    this.httpClient.get(this.baseUrl + '/outlet', {params: params}).subscribe((res: any)=>{
+	        console.log(res);
+	        if(res.data){
+		    	this.output = res.data;
+	        }
+	    },
+	    err => {
+	    	console.log('HTTP Error', err);
+	    	alert('Something went wrong');
+	    	this.output = '';
+	    });
+	}
 }
